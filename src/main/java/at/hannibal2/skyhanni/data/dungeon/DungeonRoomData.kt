b4f1467349ec07528128618a014082515fbe5c56
Cal <cwolfson58@gmail.com>
2025-05-01 23:04:41 +1000

    add position rotations

--- src/main/java/at/hannibal2/skyhanni/data/dungeon/DungeonRoomData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/dungeon/DungeonRoomData.kt
@@ -1,5 +1,7 @@
 package at.hannibal2.skyhanni.data.dungeon
 
+import at.hannibal2.skyhanni.utils.LorenzVec
+
 data class DungeonRoomData(
     val x: Int,
     val z: Int,
@@ -9,7 +11,25 @@ data class DungeonRoomData(
     val shape: RoomShape,
     val type: RoomType,
     val roomId: String,
-)
+) {
+    fun relativeToActual(pos: LorenzVec): LorenzVec {
+        return when (rotation) {
+            RoomRotation.NORTH -> LorenzVec(pos.x + x, pos.y, pos.z + z)
+            RoomRotation.EAST -> LorenzVec(-(pos.z - x), pos.y, pos.x + z)
+            RoomRotation.SOUTH -> LorenzVec(-(pos.x - x), pos.y, -(pos.z - z))
+            RoomRotation.WEST -> LorenzVec(pos.z + x, pos.y, -(pos.x - z))
+        }
+    }
+
+    fun actualToRelative(pos: LorenzVec): LorenzVec {
+        return when (rotation) {
+            RoomRotation.NORTH -> LorenzVec(pos.x - x, pos.y, pos.z - z)
+            RoomRotation.EAST -> LorenzVec(pos.z - z, pos.y, -(pos.x - x))
+            RoomRotation.SOUTH -> LorenzVec(-(pos.x - x), pos.y, -(pos.z - z))
+            RoomRotation.WEST -> LorenzVec(-(pos.z - z), pos.y, pos.x - x)
+        }
+    }
+}
 
 enum class RoomRotation {
     NORTH,
